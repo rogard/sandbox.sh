844a32286a95316488a91dccc02fd8dede24de75
rogard <1008250+rogard@users.noreply.github.com>
2022-02-22 05:31:32 -0500

    fields

--- bib/bib_fields.sh
+++ bib/bib_fields.sh
@@ -5,11 +5,78 @@ source "$(dirname "${BASH_SOURCE[0]}")/../rm_blank"
 help()
 {
     printf "s%\n%s\n%s\n%s\n"\
-    "Same as bib_fields_base.sh, but:"\
-    "- small cap keys"\
-    "- values enclosed by { and }"
+    "Takes as input a file containing one bib entry, and outputs tab separated:"\
+    "- entry type"\
+    "- identifier"\
+    "- remaining fields"
     printf "s%\n%s\n%s\n"\
-    "Syntax: bib_fields.sh [-h|--help] file"\
+    "Syntax: bib_fields_base.sh [-h|--help] file"\
     "options:"\
     "h     Print this Help."
 }
+
+# https://stackoverflow.com/a/14203146/9243116
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+      -h|--help)
+          help
+          exit 0
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+set -- "${POSITIONAL_ARGS[@]}"
+source_dir="$1"
+target_dir="$2"
+
+arr=()
+# nested, forbids:
+# readarray -t -O 1 arr < <(rm_blank "$1" | tr '\n' ' ')
+readarray -t -O 1 arr < <(rm_blank "$1")
+len="${#arr[@]}"
+(( $len == 0 )) && error_exit $(printf "length zero")
+line="${arr[1]}"
+patt='^@(.+)\{ *(.+)'
+echo "$line" | grep -qE "$patt"; exit_code="$?"
+(( exit_code == 0 ))\
+    ||  error_exit $(printf "%s head not match %s" "$line" "$patt")
+IFS=$'\t' read entry_type id < <(echo "$line" | sed -E "s/$patt/\1\t\2/")
+patt=','
+echo "$entry_type" | grep -qE "$patt"; tail_comma="$?"
+#arr[0]="$entry_type"
+arr[1]="$id"
+# tail
+line="${arr[$len]}"
+patt=' *}$'
+echo "$line" | grep -qE "$patt"; exit_code="$?"
+(( exit_code == 0 ))\
+    ||  error_exit $(printf "%s tail not match %s" "$line" "$patt")
+(( len -- ))
+
+# rest
+patt=' *?(.+[^= ]) *= *(\{.+\}) *'
+if (( $tail_comma == 0 ))
+then
+patt='^'"$patt"', *$'
+else
+patt='^ *,'"$patt"'$'
+fi   
+for (( i = 2; i <= $len; i++ ))
+do
+    line="${arr[$i]}"
+    echo "$line" | grep -qE "$patt"; exit_code="$?"
+    (( exit_code == 0 ))\
+        ||  error_exit $(printf "%s field does match %s" "$line" "$patt")
+    arr["$i"]=$(echo "$line" | sed -E "s/$patt/\1=\2/g")
+done
+
+printf '%s\t' "$entry_type"
+printf '%s\t' "${arr[@]:1:$len}"
