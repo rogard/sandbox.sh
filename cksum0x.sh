1e27284febb09210ce93bfe06bb22e9844eddccc
rogard <1008250+rogard@users.noreply.github.com>
2022-03-06 18:34:04 -0500

    removed path from cksum0x's output because one can get it from the input

--- cksum0x.sh
+++ cksum0x.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
+dirname_bash_source=$(dirname "${BASH_SOURCE[0]}")
+source "$dirname_bash_source"/error_exit
 
 help()
 {
-   echo "Hexa of cksum of file's content"
-   echo
-   echo "Syntax: cksum0x.sh [-h|--help] file "
-   echo "options:"
-   echo "h     Print this Help."
-   echo
+    echo "Syntax: cksum0x.sh [-h|--help] file_path "
+    echo "Output: Hexa of cksum of file_path's content"
+    echo "What for? For what a unique identifier may be for"
+    echo
 }
 
 # https://stackoverflow.com/a/14203146/9243116
-POSITIONAL_ARGS=()
-while [[ $# -gt 0 ]]
+positional_args=()
+while (( $# > 0 ))
 do
     case $1 in
         -h|--help)
             help
             exit 0
             ;;
+        -d|--delimiter)
+            shift
+            delimiter=$1
+            shift
+            ;;
         -*|--*)
             echo "Unknown option $1"
             exit 1
             ;;
         *)
-            printf "%s\t%s\n" "$1" $(cat "$1" | cksum | awk 'BEGIN{FS=" ";}{print $1;}' | xargs printf "%x")
+            positional_args+=("$1") # save positional arg
             shift # past argument
             ;;
     esac
 done
+
+set -- "${positional_args[@]}"
+
+(( $# == 1 )) ||\
+    error_exit $(printf "Expecting 1 positional argument, instead: %s\t" "$@")
+
+[[ -f "$1" ]] ||\
+    error_exit $(printf "Expecting a file for  %s" "$1")
+
+file_path="$1"
+
+index=$(cat "$file_path" | cksum | awk 'BEGIN{FS=" ";}{print $1;}' | xargs printf "%x\n")
+
+printf "$index"
