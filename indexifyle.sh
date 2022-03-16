46146d4c891b1381679182a960d875a0e1c48a02
rogard <1008250+rogard@users.noreply.github.com>
2022-03-16 17:00:45 -0400

    final?

--- indexifyle.sh
+++ indexifyle.sh
@@ -4,7 +4,7 @@ this_dir=$(dirname "$bash_source")
 source "$this_dir"/error_exit
 id_gen="$this_dir"/cksum0x.sh
 bool_copy=1
-info_name="info"
+info_name=".info"
 
 help()
 {
@@ -13,16 +13,12 @@ help()
            "Semantics: assigns to file1 a unique id; updates target_dir with a directory named after that id, target_id; "\
            "target_id contains:"\
            "a file having the same content as file1, and a directory named 'info'. "\
-           "Repeats for the next file."
-    echo "Other"
-    printf "-%s\n"\
-           "Intended use: remove duplicates"\
-           ".info"    
+           "Repeats with the next file."
     echo "Options:"
     printf "  --%s\n"\
            "copy=       true|false  "\
            "id-gen=     script      cksum0x.sh"\
-           "info-name=  script      info"
+           "info-name=  script      .info"
     echo "Example: find source_dir -type f -print0 | xargs -0 ./indexify.sh target_dir"
 }
 
@@ -40,38 +36,37 @@ do
             ( * ) error_exit "$this" "Wrong value for --copy=";;
         esac
         ;;
+        ( '-'* ) error_exit "$this" "Wrong option" "$1";;
         ( * ) operands+=( "$1" );;
     esac
     shift
 done
 set -- "${operands[@]}"
 
-(( bool_copy == 0 )) || echo "don't copy"
+[[ -d "$1" ]] || error_exit "$1 is not a directory"
 
-#[[ -d "$1" ]] || error_exit "$1 is not a directory"
-#
-#target_dir="$1"
-#shift
-#
-#[[ -f "$1" ]] || error_exit "$1 is not a file"
-#
-#path="$1"
-#shift
-#
-#id=$("$id_gen" "$path")
-#target_id="$target_dir/$id"
-#target_info="$target_id/$info_name"
-#target_stat="$target_info/stat"
-#
-#mkdir -p "$target_info"\
-#    && touch "$target_stat"
-# grep -vf "$target_stat" <(stat "$path") >> "$target_stat"
-#
-#if
-#    (( bool_copy == 0 ))\
-#        || [[ -z $(find "$target_id" -maxdepth 1 -type f -print -quit) ]]
-#then
-#    cp "$path" "$target_id"
-#fi
-#
-# (( $# == 0 )) || "$this" "$target_dir" "$@"
+target_dir="$1"
+shift
+
+[[ -f "$1" ]] || error_exit "$1 is not a file"
+
+path="$1"
+shift
+
+id=$("$id_gen" "$path")
+target_id="$target_dir/$id"
+target_info="$target_id/$info_name"
+target_stat="$target_info/stat"
+
+mkdir -p "$target_info"\
+    && touch "$target_stat"
+ grep -vf "$target_stat" <(stat "$path") >> "$target_stat"
+
+if
+    (( bool_copy == 0 ))\
+        || [[ -z $(find "$target_id" -maxdepth 1 -type f -print -quit) ]]
+then
+    cp "$path" "$target_id"
+fi
+
+(( $# == 0 )) || "$this" "$target_dir" "$@"
