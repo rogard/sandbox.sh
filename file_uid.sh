344b53e3530023e38a291fed20428c0621a85b01
rogard <1008250+rogard@users.noreply.github.com>
2022-03-18 02:49:19 -0400

    print

--- file_uid.sh
+++ file_uid.sh
@@ -33,10 +33,12 @@ help()
 
 print()
 {
-    find "${1}" -mindepth 1 -maxdepth 1 -type d -print\
+    target_dir="${1}"
+    printf "* %s\n" "$target_dir"
+    find "$target_dir" -mindepth 1 -maxdepth 1 -type d -print\
         | while IFS= read uid
     do
-        printf "* %s\n" $(basename "$uid"); 
+        printf "** %s\n" $(basename "$uid"); 
         sed -n '/File:/p' "$uid"/.info/stat\
             | cut -c 9-\
             | tr "\n" "\0"\
@@ -44,27 +46,34 @@ print()
     done
 }
 
+options=()
 operands=()
 while (( ${#} > 0 ))
 do
     case ${1} in
-        ( '--help' ) help; exit 0;;
-        ( '--print'* ) bool_print=0;;
-        ( '--uid-gen='* ) uid_gen="${1#*=}";;
-        ( '--info-dir='* ) info_dir="${1#*=}";;
-        ( '--info-do='* ) info_do="${1#*=}";;
-        ( '--copy='* )
-        case ${1#*=} in
-            ( 'false' ) bool_copy=1;;
-            ( 'true' ) bool_copy=0;;
-            ( * ) error_exit "$this" "Wrong value for --copy=";;
-        esac
-        ;;
-        ( '--rename='* )
-        case ${1#*=} in
-            ( 'false' ) bool_rename=1;;
-            ( 'true' ) bool_rename=0;;
-            ( * ) error_exit "$this" "Wrong value for --copy=";;
+        ( '--'* )
+        option="${1}"
+        options+=( "${option}" )
+        case ${option} in        
+            ( '--help' ) help; exit 0;;
+            ( '--print'* ) bool_print=0;;
+            ( '--uid-gen='* ) uid_gen="${1#*=}";;
+            ( '--info-dir='* ) info_dir="${1#*=}";;
+            ( '--info-do='* ) info_do="${1#*=}";;
+            ( '--copy='* )
+            case ${1#*=} in
+                ( 'false' ) bool_copy=1;;
+                ( 'true' ) bool_copy=0;;
+                ( * ) error_exit "$this" "Wrong value for --copy=";;
+            esac
+            ;;
+            ( '--rename='* )
+            case ${1#*=} in
+                ( 'false' ) bool_rename=1;;
+                ( 'true' ) bool_rename=0;;
+                ( * ) error_exit "$this" "Wrong value for --copy=";;
+            esac
+            ;;
         esac
         ;;
         ( '-'* ) error_exit "$this" "Wrong option" "$1";;
@@ -82,51 +91,51 @@ shift
 if (( $bool_print == 0 ))
 then
     print "$target_root"
-    exit 0
-fi
+else
+    
+    [[ -f "$1" ]] || error_exit "$0 expects a file; got $1"
 
-[[ -f "$1" ]] || error_exit "$0 expects a file; got $1"
+    path="$1"
+    shift
 
-path="$1"
-shift
+    id=$("$SHELL" -c "$uid_gen" "$SHELL" "$path")\
+        || error_exit "$this->$id"
+    target_dir="$target_root/$id"
 
-id=$("$SHELL" -c "$uid_gen" "$SHELL" "$path")\
-    || error_exit "$this->$id"
-target_dir="$target_root/$id"
+    target_info="$target_dir/$info_dir"
+    target_stat="$target_info/stat"
+    target_path="$target_dir"/$(basename "$path")
 
-target_info="$target_dir/$info_dir"
-target_stat="$target_info/stat"
-target_path="$target_dir"/$(basename "$path")
+    mkdir -p "$target_info"\
+        && touch "$target_stat"\
+        && grep -vf "$target_stat" <(stat "$path") >> "$target_stat"
 
-mkdir -p "$target_info"\
-    && touch "$target_stat"\
-    && grep -vf "$target_stat" <(stat "$path") >> "$target_stat"
+    [[ -z "$info_do" ]] || "$SHELL" -c "$info_do" "$SHELL" "$target_info" "$target_stat"
 
-[[ -z "$info_do" ]] || "$SHELL" -c "$info_do" "$SHELL" "$target_info" "$target_stat"
-
-if
-    (( bool_copy == 0 ))
-then
-
-    mapfile -d '' array < <(find "$target_dir" -maxdepth 1 -type f -print0)
-    for other_path in "${array[@]}"
-    do
-        other_id=$("$SHELL" -c "$uid_gen" "$SHELL" "$other_path") || error_exit "$this->$other_id"
-
-        if [[ $other_id == $id ]]
-        then
-            if (( bool_rename == 0 ))
-            then
-                [[ "$other_path" == "$target_path" ]] || mv "$other_path" "$target_path"
-            fi
-            bool_copy=1 # cancel copy
-            break
-        fi
-    done    
-    if (( bool_copy == 0 ))
+    if
+        (( bool_copy == 0 ))
     then
-        cp "$path" "$target_dir"
+
+        mapfile -d '' array < <(find "$target_dir" -maxdepth 1 -type f -print0)
+        for other_path in "${array[@]}"
+        do
+            other_id=$("$SHELL" -c "$uid_gen" "$SHELL" "$other_path") || error_exit "$this->$other_id"
+
+            if [[ $other_id == $id ]]
+            then
+                if (( bool_rename == 0 ))
+                then
+                    [[ "$other_path" == "$target_path" ]] || mv "$other_path" "$target_path"
+                fi
+                bool_copy=1 # cancel copy
+                break
+            fi
+        done    
+        if (( bool_copy == 0 ))
+        then
+            cp "$path" "$target_dir"
+        fi
     fi
 fi
 
-(( $# == 0 )) || "$this" "$target_root" "$@"
+(( $# == 0 )) || "$this" "${options[@]}" "$@"
