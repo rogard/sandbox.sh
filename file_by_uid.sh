df06f08c2d681bbdd194352d9413706f244c7adf
rogard <1008250+rogard@users.noreply.github.com>
2022-03-25 03:16:59 -0400

    typo in help

--- file_by_uid.sh
+++ file_by_uid.sh
@@ -17,7 +17,7 @@
 # $ find '/home/er/Documents/record/BG' -type f -size +0 -print0\
 #    | xargs -0 './recursextract_do.sh' './file_by_uid.sh\
 #    --info-do+='\''find $(dirname "$1")-size 0 -exec cp {} "$2" \;'\''\
-#    '/home/er/uid'  "$1";'
+#    '/home/er/uid' "$1";'
 # =========================================================================
 this="${BASH_SOURCE[0]}"
 this_dir=$(dirname "$bash_source")
